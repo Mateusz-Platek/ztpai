d402a245946cd25aecbb760d973c7fac70445378
Mateusz-Platek <m.platek1705@gmail.com>
2024-05-06 17:28:55 +0200

    Added footer

--- automarket24-frontend/src/app/(logging)/layout.tsx
+++ automarket24-frontend/src/app/(logging)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from '@/components/header';
+import Footer from '@/components/footer';
 
 export default function Layout({
     children
@@ -9,7 +10,7 @@ export default function Layout({
     return (
         <>
             <Header logged={true} />
-            <div className="mt-12 px-8 2xl:px-64">
+            <div className="my-10 px-8 2xl:px-6 w-full">
                 {children}
             </div>
         </>
