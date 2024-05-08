d1f5a141215b3341a79c4598da73b4e1821be203
Mateusz-Platek <m.platek1705@gmail.com>
2024-05-08 14:09:45 +0200

    Updated filter chain

--- automarket24-backend/src/main/java/org/example/automarket24backend/security/SecurityFilterChainConfiguration.java
+++ automarket24-backend/src/main/java/org/example/automarket24backend/security/SecurityFilterChainConfiguration.java
@@ -3,6 +3,7 @@ package org.example.automarket24backend.security;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,22 +21,22 @@ public class SecurityFilterChainConfiguration {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        request -> request.anyRequest().permitAll()
-//                                .requestMatchers("/swagger-ui/**", "/api-docs/**", "/v2/api-docs",
-//                                        "/configuration/ui", "/swagger-resources/**", "/api-docs-ui.html",
-//                                        "/configuration/security", "/swagger-ui.html", "/webjars/**",
-//                                        "/v3/api-docs/**")
-//                                .permitAll()
-//                                .requestMatchers(HttpMethod.GET,
-//                                        "/features", "/colors", "/fuel-types", "/generations",
-//                                        "/transmissions", "/drivetrains", "/models", "/makes",
-//                                        "/offers/**", "/body-types", "/conditions", "/users/**"
-//                                ).permitAll()
-//                                .requestMatchers("/login", "/register").permitAll()
-//                                .requestMatchers(HttpMethod.DELETE,"/offers/**").hasAnyAuthority("Admin", "Private")
-//                                .requestMatchers(HttpMethod.POST,"/offers/**").hasAnyAuthority("Admin", "Private")
-//                                .requestMatchers("/users/**").hasAuthority("Admin")
-//                                .anyRequest().authenticated()
+                        request -> request
+                                .requestMatchers("/swagger-ui/**", "/api-docs/**", "/v2/api-docs",
+                                        "/configuration/ui", "/swagger-resources/**", "/api-docs-ui.html",
+                                        "/configuration/security", "/swagger-ui.html", "/webjars/**",
+                                        "/v3/api-docs/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET,
+                                        "/features", "/colors", "/fuel-types", "/generations",
+                                        "/transmissions", "/drivetrains", "/models", "/makes",
+                                        "/offers/**", "/body-types", "/conditions", "/users/**"
+                                ).permitAll()
+                                .requestMatchers("/login", "/register").permitAll()
+                                .requestMatchers(HttpMethod.DELETE,"/offers/**").hasAnyAuthority("Admin", "Private")
+                                .requestMatchers(HttpMethod.POST,"/offers/**").hasAnyAuthority("Admin", "Private")
+                                .requestMatchers("/users/**").hasAuthority("Admin")
+                                .anyRequest().authenticated()
                 )
                 .sessionManagement(
                         session -> session
